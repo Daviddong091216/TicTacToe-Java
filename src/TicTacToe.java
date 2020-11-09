04d0befc9031e2f2264450dc97b4d8b57da15746
David Dong <daviddong201712@gmail.com>
2020-11-09 15:32:16 -0600

    Adds some mothods and construtor.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
 
-public class TicTacToe {
+
+public class TicTacToe implements ActionListener{
+    TicTacToe(){
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public void firstTurn(){
+
+    }
+    public void check(){
+
+    }
+    public void xWins(int a, int b, int c){
+
+    }
+    public void oWins(int a, int b, int c){
+
+    }
 
 }
