691ee195749d59e2c7f948f40c36cdf566c6addc
David Dong <daviddong201712@gmail.com>
2020-11-09 16:17:55 -0600

    Creats actionPerformed and firstTurn methods.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -4,9 +4,9 @@ import java.util.*;
 import javax.swing.*;
 
 
-public class TicTacToe implements ActionListener{
+public class TicTacToe implements ActionListener {
 
-    Random random=new Random();
+    Random random = new Random();
     JFrame frame = new JFrame();
     JPanel titlePanel = new JPanel();
     JPanel buttonPanel = new JPanel();
@@ -15,30 +15,39 @@ public class TicTacToe implements ActionListener{
     boolean player1Turn;
 
 
-
-    TicTacToe(){
+    TicTacToe() {
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,800);
-        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setSize(800, 800);
+        frame.getContentPane().setBackground(new Color(50, 50, 50));
         frame.setVisible(true);
 
-        textField.setBackground(new Color(25,25,25));
-        textField.setForeground(new Color(25,255,0));
-        textField.setFont(new Font("Ink Free",Font.BOLD,75));
+        textField.setBackground(new Color(25, 25, 25));
+        textField.setForeground(new Color(25, 255, 0));
+        textField.setFont(new Font("Ink Free", Font.BOLD, 75));
         textField.setHorizontalAlignment(JLabel.CENTER);
         textField.setText("Tic-Tac-Toe-For-Miley");
         textField.setOpaque(true);
 
         titlePanel.setLayout(new BorderLayout());
-        titlePanel.setBounds(0,0,800,100);
+        titlePanel.setBounds(0, 0, 800, 100);
+
+        buttonPanel.setLayout(new GridLayout(3, 3));
+        buttonPanel.setBackground(new Color(150, 150, 150));
+
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton();
+            buttonPanel.add(buttons[i]);
+            buttons[i].setFont(new Font("MV Boil", Font.BOLD, 120));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+        }
 
         titlePanel.add(textField);
-        frame.add(titlePanel);
+        frame.add(titlePanel, BorderLayout.NORTH);
+        frame.add(buttonPanel);
 
-
-
-
+        firstTurn();
 
     }
 
@@ -46,17 +55,59 @@ public class TicTacToe implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
 
-    }
-    public void firstTurn(){
+        for (int i = 0; i < 9; i++) {
+            if (e.getSource() == buttons[i]) {
+                if (player1Turn) {
+                    if (buttons[i].getText() == "") {
+                        buttons[i].setForeground(new Color(255, 0, 0));
+                        buttons[i].setText("X");
+                        player1Turn = false;
+                        textField.setText("O turn");
+                        check();
+                    }
+                } else {
+                    if (buttons[i].getText() == "") {
+                        buttons[i].setForeground(new Color(0, 0, 255));
+                        buttons[i].setText("O");
+                        player1Turn = true;
+                        textField.setText("X turn");
+                        check();
+                    }
+                }
+            }
+        }
+
 
     }
-    public void check(){
+
+    public void firstTurn() {
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        if (random.nextInt(2) == 0) {
+            player1Turn = true;
+            textField.setText("X turn");
+        } else {
+            player1Turn = false;
+            textField.setText("O turn");
+        }
 
     }
-    public void xWins(int a, int b, int c){
+
+    public void check() {
 
     }
-    public void oWins(int a, int b, int c){
+
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void oWins(int a, int b, int c) {
 
     }
 
